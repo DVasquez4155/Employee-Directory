07a0a0e23518048afbc31478c705ad4518e100aa
Daniel V <main@dvasquez4155.com>
2020-05-11 18:57:38 -0700

    Functional Sorting

--- src/pages/Index.js
+++ src/pages/Index.js
@@ -1,10 +1,103 @@
-import React from "react";
+import React, { Component } from "react"
 import Hero from "../components/Hero";
 import Container from "../components/Container";
 import Row from "../components/Row";
 import Col from "../components/Col";
+import EmployeeCard from "../components/EmployeeCard";
+import {Dropdown} from 'react-bootstrap';
+import employees from "../employees.json";
+import departments from "../departments.json";
+import roles from "../roles.json";
 
-function About() {
+class Index extends Component {
+  state = {
+    employees,
+    departments,
+    roles
+  };
+  findEmployeeById = id => {
+    var result;
+    this.state.employees.forEach(employee => {
+      if (employee.id === id) {
+        result = employee;
+      }
+    })
+    if (result) {
+      return (result.first_name + " " + result.last_name)
+    }
+    else {
+      return "N/A"
+    }
+  }
+  findRoleById = id => {
+    var result;
+    this.state.roles.forEach(role => {
+      if (role.id === id) {
+        result = role;
+      }
+    })
+    return result;
+  }
+  findDepartmentById = id => {
+    var result;
+    this.state.departments.forEach(department => {
+      if (department.id === id) {
+        result = department;
+      }
+    })
+    return result;
+  }
+  loadEmployees = () => {employees.map(employee => {
+    employee.name = employee.first_name + " " + employee.last_name;
+    employee.title = this.findRoleById(employee.role_id).title;
+    employee.department = this.findDepartmentById(this.findRoleById(employee.role_id).department_id).name
+    employee.salary = this.findRoleById(employee.role_id).salary
+    employee.manager = this.findEmployeeById(employee.manager_id)
+    return null;
+  })}
+  formatter = new Intl.NumberFormat('en-US', {
+    style: 'currency',
+    currency: 'USD',
+    minimumFractionDigits: 2
+  })
+  handleSelect = e => {
+    const list = this.state.employees;
+    const rules = {a:null,b:null};
+    switch(parseInt(e)) {
+      case 0:
+        rules.a = "id";
+        rules.b = "name";
+        break;
+      case 1:
+        rules.a = "first_name";
+        rules.b = "last_name";
+        break;
+      case 2:
+        rules.a = "title";
+        rules.b = "name";
+        break;
+      case 3:
+        rules.a = "department";
+        rules.b = "name";
+        break;
+      case 4:
+        rules.a = "salary";
+        rules.b = "name";
+        break;
+      case 5:
+        rules.a = "manager";
+        rules.b = "name";
+        break;
+      default:
+        rules.a = "id";
+        rules.b = "name";
+    }
+    var sortedEmployees;
+      sortedEmployees = list.sort((a, b) => (a[rules.a] > b[rules.a]) ? 1 : (a[rules.a] === b[rules.a]) ? ((a[rules.b] > b[rules.b]) ? 1 : -1) : -1 )
+    this.setState({ sortedEmployees });
+  }
+  render() {
+    this.loadEmployees();
   return (
     <div>
       <Hero backgroundImage="https://ak.picdn.net/shutterstock/videos/4388393/thumb/2.jpg">
@@ -14,50 +107,37 @@ function About() {
         <Row>
           <Col size="md-12">
             <h1>Welcome To Employee Directory!</h1>
+            <Dropdown onSelect={this.handleSelect}>
+                <Dropdown.Toggle variant="dark" id="dropdown-basic">
+                Sort By
+                </Dropdown.Toggle>
+                <Dropdown.Menu>
+                <Dropdown.Item eventKey="0">ID</Dropdown.Item>
+                <Dropdown.Item eventKey="1">Name</Dropdown.Item>
+                <Dropdown.Item eventKey="2">Title</Dropdown.Item>
+                <Dropdown.Item eventKey="3">Department</Dropdown.Item>
+                <Dropdown.Item eventKey="4">Salary</Dropdown.Item>
+                <Dropdown.Item eventKey="5">Manager</Dropdown.Item>
+                </Dropdown.Menu>
+            </Dropdown>
           </Col>
         </Row>
         <Row>
-          <Col size="md-12">
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc aliquet diam tortor, id
-              consequat mauris ullamcorper eu. Orci varius natoque penatibus et magnis dis
-              parturient montes, nascetur ridiculus mus. Pellentesque et dui id justo finibus
-              sollicitudin at et metus. Ut feugiat tellus nec metus commodo, sed suscipit nisi
-              gravida. Duis eget vestibulum quam, ut porttitor sem. Donec sagittis mi sollicitudin
-              turpis semper, et interdum risus lobortis. Vestibulum suscipit nunc non egestas
-              tristique. Proin hendrerit efficitur malesuada. Mauris lorem urna, sodales accumsan
-              quam non, tristique tempor erat. Nullam non sem facilisis, tempus tortor sit amet,
-              volutpat nisl. Ut et turpis non nunc maximus mollis a vitae tortor. Pellentesque
-              mattis risus ac quam laoreet cursus. Praesent suscipit orci neque, vestibulum
-              tincidunt augue tincidunt non. Duis consequat mattis tortor vitae mattis.
-            </p>
-            <p>
-              Phasellus at rutrum nisl. Praesent sed massa ut ipsum bibendum porttitor. Sed
-              malesuada molestie velit ac viverra. Quisque a ullamcorper purus. Curabitur luctus mi
-              ac mi hendrerit semper. Nulla tincidunt accumsan lobortis. Mauris convallis sapien non
-              nibh porta accumsan. Nunc volutpat tempus porttitor. Nunc congue dictum egestas.
-              Aliquam blandit mi eu urna scelerisque, vitae volutpat ligula ultricies. Maecenas vel
-              porta augue. Fusce mauris ex, dignissim et lacinia ut, tempus eget nibh.
-            </p>
-            <p>
-              Etiam ut massa efficitur, gravida sapien non, condimentum sapien. Suspendisse massa
-              tortor, facilisis in neque sit amet, scelerisque elementum tortor. Nullam eget nibh
-              sit amet odio lobortis ullamcorper. Nulla bibendum magna nec sem pulvinar lobortis.
-              Mauris et imperdiet urna, vitae lobortis dui. Nunc elementum elit mi, non mattis enim
-              congue at. Proin mi lectus, ullamcorper id hendrerit eu, ultricies vitae lacus. Nunc
-              vehicula, erat eget laoreet condimentum, felis ante malesuada leo, nec efficitur diam
-              nisi eget nisi. Cras arcu lacus, tristique in bibendum vitae, elementum eget lorem.
-              Maecenas vestibulum volutpat orci eu pharetra. Praesent vel blandit ante, nec faucibus
-              libero. Sed ultrices lorem ex, eu facilisis libero convallis ac. Vivamus id dapibus
-              eros. Nullam tempor sem rhoncus porta semper. Proin bibendum vulputate nisl, fringilla
-              interdum elit pulvinar eu. Quisque vitae quam dapibus, vestibulum mauris quis, laoreet
-              massa.
-            </p>
-          </Col>
+        {this.state.employees.map(employee => (
+          <EmployeeCard
+            name={employee.name}
+            id={employee.id}
+            key={employee.id}
+            title={employee.title}
+            department={employee.department}
+            salary={this.formatter.format(employee.salary)}
+            manager={employee.manager}
+          />
+        ))}
         </Row>
       </Container>
     </div>
   );
-}
+}}
 
-export default About;
+export default Index;
